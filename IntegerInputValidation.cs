b5443b5629ce6dd4818c5587c56bf4dcafda189f
Shaheen Saleem <141652787+Shaheen-Saleem@users.noreply.github.com>
2025-12-01 09:49:33 +0530

    Add integer input validation logic
    Implemented input validation for an integer between 5 and 10 using a do-while loop.

---
+++ IntegerInputValidation.cs
@@ -0,0 +1,46 @@
+/*
+Here are the conditions that your first coding project must implement:
+
+    Your solution must include either a do-while or while iteration.
+
+    Before the iteration block: your solution must use a Console.WriteLine() statement to prompt the user for an integer value between 5 and 10.
+
+    Inside the iteration block:
+        Your solution must use a Console.ReadLine() statement to obtain input from the user.
+        Your solution must ensure that the input is a valid representation of an integer.
+        If the integer value isn't between 5 and 10, your code must use a Console.WriteLine() statement to prompt the user for an integer value between 5 and 10.
+        Your solution must ensure that the integer value is between 5 and 10 before exiting the iteration.
+
+    Below (after) the iteration code block: your solution must use a Console.WriteLine() statement to inform the user that their input value has been accepted.
+*/
+
+string? readInput;
+Console.WriteLine("Enter an integer value between 5 and 10: ");
+bool validInput = false;
+int numericInput = 0;
+
+do
+{
+    readInput = Console.ReadLine();
+    validInput = int.TryParse(readInput, out numericInput);
+    if (validInput)
+    {
+        if ((numericInput >= 5) && (numericInput <= 10))
+        {
+            validInput = true;
+        }
+        else
+        {
+            Console.WriteLine($"You entered {numericInput}. Please enter a number between 5 and 10");
+            validInput = false;
+            continue;
+        }
+    }
+    else
+    {
+        Console.WriteLine("Sorry, you entered an invalid number, please try again");
+        continue;
+    }
+} while(validInput == false);
+
+Console.WriteLine($"Your input value ({numericInput}) has been accepted!");
